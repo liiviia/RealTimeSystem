f1013263029a75be6b77af1c855a0824bc2cf267
= <=>
2025-07-28 21:35:12 +0200

    react dom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Send from './Send';
 import DisplayPage from './DisplayPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Send />
-    <DisplayPage />
+    <Router>
+      <Routes>
+        <Route path="/send" element={<Send />} />
+        <Route path="/display" element={<DisplayPage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
-
